85a54219dadc5010e4ffe882448e27808b7b45af
Subham2804 <subhams2804@gmail.com>
2026-04-20 15:45:26 +0530

    layout skeleton

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+pub mod error;
+pub mod ingest;
+pub mod pipeline;
+pub mod utils;
+pub mod normalize;
+pub mod summarize;
+pub mod delivery;
---
+++ src/normalize/mod.rs
@@ -0,0 +1,2 @@
+// TODO: Implement the pipeline module
+pub fn placeholder() {}
